f23b7c74f9f83dd365320166f6d09cab65cd8c97
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2019-09-27 09:14:53 +0100

    Some js cleanup

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,7 @@
 // Coloured squares
 let list = document.querySelectorAll('.color-square');
 for (let i = 0; i < list.length; i++) {
-    console.log(list[i].getAttribute('data-color'));
     let color = list[i].getAttribute('data-color');
     list[i].style.backgroundColor = color;
+    console.log(color);
 }
